1918418cdb8bda16a7f4049fc9163f7ab17febfc
BuildTools <marco.firsteye@gmail.com>
2017-03-06 17:48:37 -0800

    Changed Lab 3 name

---
+++ Lab3 - MIPS ALU/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Marco/Dropbox/School/WINTER 2017/161L/Labs/Lab3-MIPS_ALU/alutb_isim_beh.exe" -prj "C:/Users/Marco/Dropbox/School/WINTER 2017/161L/Labs/Lab3-MIPS_ALU/alutb_beh.prj" "work.alutb" "work.glbl" 
